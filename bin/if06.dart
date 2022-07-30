b3e13103d976312c81c97b2a0df2483b701bc207
Begzod <ebegzod477@gmail.com>
2022-07-30 22:24:45 +0500

    code06

--- bin/if06.dart
+++ bin/if06.dart
@@ -6,5 +6,25 @@ Args:
 Returns:
     int: return answer. 
 */
+int func(int n) {
+  int x1, x2, x3, x4, x5;
+  x1 = n % 10;
+  x2 = n ~/ 10 % 10;
+  x3 = n ~/ 10 ~/ 10 % 10;
+  x4 = n ~/ 10 ~/ 10 ~/ 10 ~/ 10 % 10;
+  x5 = n ~/ 10 ~/ 10 ~/ 10 ~/ 10 ~/ 10 % 10;
+  if (x1 > x2) {
+    return 1;
+  } else if (x2 > x3) {
+    return 2;
+  } else if (x3 > x4) {
+    return 3;
+  } else if (x4 > x5) {
+    return 4;
+  }
+  return 5;
+}
+
 void main() {
+  print(func(68309));
 }
