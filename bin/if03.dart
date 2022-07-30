badfc34bf5a644e5aabd769eb24c82bb7dee4d6c
Begzod <ebegzod477@gmail.com>
2022-07-30 13:00:50 +0500

    code03

--- bin/if03.dart
+++ bin/if03.dart
@@ -7,5 +7,19 @@ Args:
     c: Third number.
 Returns:
     int: return answer. */
+int func(int first, second, third) {
+  if (first < second) {
+    if (first > third) {
+      return first;
+    }
+  } else if (third < first) {
+    if (third > second) {
+      return third;
+    }
+  }
+  return second;
+}
+
 void main() {
+  print(func(9, 12, 6));
 }
